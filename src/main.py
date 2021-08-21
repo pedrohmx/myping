64fe1d526f000c2a6e62458c2eb04778d250ab43
Pedro Xavier <pedrohx@yahoo.com.br>
2021-08-21 12:23:29 -0300

    finished create_packet with dummy checksum

--- src/main.py
+++ src/main.py
@@ -15,7 +15,9 @@ def get_lookup(host: str) -> Tuple(str,str):
     addr = socket.gethostbyname_ex(host)[2][0]
     hostname = socket.gethostbyaddr(addr)[0]
     return (hostname, addr)
-def checksum(): pass
+def checksum():
+	# TODO:
+	return 0
 def ping(): pass
 def create_packet(id: int, seq: int = 1):
 	# header is
@@ -24,7 +26,10 @@ def create_packet(id: int, seq: int = 1):
 	# checksum(16)
 	# id(16)
 	# sequence(16)
-	#header = struct.pack('bbHHH',...)
+	header = struct.pack('bbHHH', ICMP_ECHO_REQUEST,0,0,id,seq)
+	data = 64 * b'i'
+	pkg_checksum = checksum(header + data)
+	header = struct.pack('bbHHH', ICMP_ECHO_REQUEST,0,pkg_checksum,id,seq)
 	pass
 
 if __name__ == '__main__':
