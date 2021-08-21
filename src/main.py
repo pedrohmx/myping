0657fac322207c7bc029fc128514c363c4638d25
Pedro Xavier <pedrohx@yahoo.com.br>
2021-08-21 12:20:48 -0300

    fixed shameful typo

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import struct
 from typing import Tuple
 
 #from /usr/include/linux/icmp.h
-ICMP_ECHO = 8
+ICMP_ECHO_REQUEST = 8
 ICMP_CODE = socket.getprotobyname('icmp')
 
 
@@ -15,7 +15,7 @@ def get_lookup(host: str) -> Tuple(str,str):
     addr = socket.gethostbyname_ex(host)[2][0]
     hostname = socket.gethostbyaddr(addr)[0]
     return (hostname, addr)
-def chacksum(): pass
+def checksum(): pass
 def ping(): pass
 def create_packet(id: int, seq: int = 1):
 	# header is
