869b0a3209f11662fd18eea59dd1efe82ef3aff8
pedrohmx <pedrohx@yahoo.com.br>
2021-08-30 16:59:49 -0300

    Current progres

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import time
 import socket
 import struct
 import select
-from typing import Tuple, Union
+import signal
+from typing import List, Tuple, Union
 
 #from /usr/include/linux/icmp.h
 ICMP_ECHO_REQUEST = 8
@@ -16,11 +17,11 @@ def get_lookup(host: str) -> Union[Tuple[str,str], None]:
 		hostname = socket.gethostbyaddr(addr)[0]
 		return (hostname, addr)
 	except socket.gaierror as gai:
-		print('Can not resolve host: name or service not known.')
+		print('Name or service not known.')
 		#print(gai)
 		return None
 	except Exception as err:
-		print('Unknown exception')
+		print('Could not reach name or service: unknown exception')
 		print(err)
 		return None
 
@@ -33,29 +34,29 @@ def checksum(data: bytes):
 	x = x & 0xFFFF
 	return x
 
-def create_packet(pkg_id: int = DEFAULT_ID, seq: int = 1, data = None):
+def create_packet(pkt_id: int = DEFAULT_ID, seq: int = 1, data = None):
 	# header is
 	# type(8 bits) # code(8 bits) # checksum(16 bits)
 	# id(16 bits)                 # sequence(16 bits)
-	header = struct.pack('bbHHH', ICMP_ECHO_REQUEST,0,0,pkg_id,seq)
+	header = struct.pack('bbHHH', ICMP_ECHO_REQUEST,0,0,pkt_id,seq)
 	if data is None:
 		data = (64 - 8) * b'i'
-	pkg_checksum = checksum(header + data)
-	header = struct.pack('bbHHH', ICMP_ECHO_REQUEST,0,pkg_checksum,pkg_id,seq)
+	pkt_checksum = checksum(header + data)
+	header = struct.pack('bbHHH', ICMP_ECHO_REQUEST,0,pkt_checksum,pkt_id,seq)
 	return header + data
 
 # ping once
-def ping_once():
-	pacote = create_packet(seq=1)
+def ping_once(host_name: str, host_addr: str, pkt_id=DEFAULT_ID, seq=1, timeout_ms=1000):
+	pacote = create_packet(pkt_id=pkt_id, seq=seq)
 	with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
-		sock.connect( ('www.google.com',80) )
+		# Connect
+		sock.connect( (host_name,80) )
 		sock.send(pacote)
 		
 		start_time = time.time()
 
 		### RECEIVE
-		# timeout 5
-		time_left = timeout = 1
+		time_left = timeout_ms / 1000
 		while True:
 			started_select = time.time()
 			ready = select.select([sock],[],[], time_left)
@@ -71,18 +72,19 @@ def ping_once():
 
 			ip_header = recv[:20]
 
-			ip_version, ip_type_svc, ip_length, ip_id, ip_flags, ip_TTL, \
+			ip_version, ip_type_svc, ip_length, ip_id, ip_flags, ttl, \
 			ip_protocol, ip_checksum, ip_src, ip_dest = struct.unpack(
 				"!BBHHHBBHII", ip_header)
 
 			icmp_header = recv[20:28]
 
-			icmp_type, icmp_code, icmp_checksum, icmp_pkg_id, icmp_seq \
+			icmp_type, icmp_code, icmp_checksum, icmp_pkt_id, icmp_seq \
 				= struct.unpack('bbHHH', icmp_header)
 
-			# dummy 0 id, change later
-			if (icmp_type != ICMP_ECHO_REQUEST) and (icmp_pkg_id == DEFAULT_ID):
-				return (time_received - start_time), ( len(recv) - 20 ), icmp_seq, ip_TTL
+			if (icmp_type != ICMP_ECHO_REQUEST) and (icmp_pkt_id == DEFAULT_ID):
+				delta_ms = (time_received - start_time) * 1000
+				print(f'{len(recv)-20} bytes from {host_name} ({host_addr}): {icmp_seq=} {ttl=} time={delta_ms:.3f} ms')
+				return delta_ms
 
 			time_left -= time_in_select
 			if time_left <= 0:
@@ -90,15 +92,70 @@ def ping_once():
 				return None
 			pass
 
+
+
+def stats(timing_list: List[float], packets_lost: int):
+	rcvd_pkt = len(timing_list)
+	total_pkt = rcvd_pkt + packets_lost
+	percent = (packets_lost / total_pkt) * 100
+	print(f'\n{total_pkt} packets transmitted, {rcvd_pkt} received, {percent}% packet loss.')
+	rtt_min = min(timing_list)
+	rtt_avg = sum(timing_list) / len(timing_list)
+	rtt_max = max(timing_list)
+	print(f'rtt min/avg/max = {rtt_min:.3f}/{rtt_avg:.3f}/{rtt_max:3f} ms.')
+	pass
+
+def _sigint_handler(signum,frame):
+	'''Handle exit via SIG INT'''
+	global timing_list
+	global lost_packets
+	stats(timing_list=timing_list, packets_lost=lost_packets)
+	print('\nTerminated with signal SIGINT')
+	exit(0)
+	pass
+
+#timing_list = []
+#lost_packets = 0
+
 if __name__ == '__main__':
-	#print(ping_once())
-	res = ping_once()
+	signal.signal(signal.SIGINT, _sigint_handler)
+	####    argparse
+	HOSTNAME = 'unioeste.br'
+	COUNT = 5
+	TIMEOUT_MS = 1000
+	####    ####    ####    ####
+
+	res = get_lookup(HOSTNAME)
 	if res is None:
 		exit(0)
-	ping_time, data_len, icmp_seq, ip_ttl = res
-	print(f'Ping')
-	print(f'{ping_time = }')
-	print(f'{data_len = }')
-	print(f'{icmp_seq = }')
-	print(f'{ip_ttl = }')
+	host_name, host_addr = res
+
+	print(f'PING {host_name} ({host_addr}) with 56 (84: ip(20) + icmp(8) + payload) bytes of data.\n')
+	
+	global timing_list
+	global lost_packets
+	timing_list = []
+	lost_packets = 0
+
+	seq = 1
+	while True:
+		# ping
+		res = ping_once(
+			host_name=host_name, host_addr=host_addr, seq=seq, timeout_ms=TIMEOUT_MS)
+		# store result
+		if res is None:
+			lost_packets += 1
+		else:
+			timing_list.append(res)
+		# check loop
+		time.sleep(0.5)
+		if COUNT is None:
+			continue
+		if seq >= COUNT:
+			break
+		else:
+			seq += 1
+		pass
+	# stats
+	stats(timing_list=timing_list, packets_lost=lost_packets)
 	pass
